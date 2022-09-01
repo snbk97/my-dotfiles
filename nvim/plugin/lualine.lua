705a77a81716a3dd4569846cbbe775e19d74e0e0
snbk97 <snbk97@gmail.com>
2022-09-01 09:35:03 +0530

    relative file path in lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,12 +1,18 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    -- theme = 'auto',
+    theme = 'auto',
     --theme = 'horizon',
     -- theme = 'codedark',
-    theme = 'onedark',
+    -- theme = 'onedark',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     globalstatus = true,
+  },
+  sections = {
+    lualine_c = { {
+      'filename',
+      path = 1 -- 0: Just the filename, 1: Relative, 2, 3: Absolute path
+    } },
   }
 }
